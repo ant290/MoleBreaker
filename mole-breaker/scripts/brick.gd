d3f4b0246dd884f4226052237fe95531257be81b
Ant Perry <ant290@live.co.uk>
2025-12-27 17:08:46 +0000

    little tidyup

--- mole-breaker/scripts/brick.gd
+++ mole-breaker/scripts/brick.gd
@@ -14,7 +14,6 @@ var health : int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health = starting_health
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
